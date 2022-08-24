64843ce15ef2afc7ecc4a9cc8ed3231cedb84a4e
Ajay263 <junioralexio607@gmail.com>
2022-08-24 09:45:10 +0200

    first version

---
+++ src/com/ajay545/notes.java
@@ -0,0 +1,68 @@
+package com.ajay545;
+
+public class notes {
+
+    public static void main(String[] args) {
+
+        // This represents  a single line comment
+
+
+
+        /* This represents
+
+
+        a multi
+
+
+        line
+
+        comment
+
+
+        */
+
+
+//        VARIABLES
+
+        /*     Variables are containers for storing data values
+
+
+        Declaring (Creating) Variables
+
+
+            To create a variable, you must specify the type and assign it a value:
+
+            EXAMPLES OF THE TYPES THAT WE HAVE IN JAVA:
+
+                String - stores text, such as "Hello". String values are surrounded by double quotes
+                int - stores integers (whole numbers), without decimals, such as 123 or -123
+                float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+                char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+                boolean - stores values with two states: true or false
+
+                    type variable = value;
+
+                   String name = "John";
+                   System.out.println(name);
+
+
+                   int myNum = 15;
+                   System.out.println(myNum);
+
+
+
+Final Variables
+
+You can add the final keyword if you don't want to overwrite existing values OF A VARIABLE
+
+
+
+
+
+
+
+
+
+         */
+    }
+}
